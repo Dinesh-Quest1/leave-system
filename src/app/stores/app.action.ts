38cfe05b1bd6d1525efcc3e52d133a5b0c52e9dc
Dinesh_git_lab <dinesh.kumar@vhitechlab.com>
2025-01-04 11:48:49 +0530

    changes: table styling and api server for data loading

--- src/app/stores/app.action.ts
+++ src/app/stores/app.action.ts
@@ -1,14 +1,20 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 
 const ADD_USER = 'ADD USER';
+const LOAD_USER = 'LOAD USER';
 const UPDATE_USER = 'UPDATE USER';
 const ADD_LEAVE = 'ADD LEAVE';
+const LOAD_LEAVE = 'LOAD LEAVE';
 const UPDATE_LEAVE = 'UPDATE LEAVE';
 
-export const addUser = createAction(ADD_USER);
+export const addUser = createAction(ADD_USER, props<any>());
 
-export const updateUser = createAction(UPDATE_USER);
+export const loadUser = createAction(LOAD_USER, props<any>());
 
-export const addLeave = createAction(ADD_LEAVE);
+export const updateUser = createAction(UPDATE_USER, props<any>());
 
-export const updateLeave = createAction(UPDATE_LEAVE);
+export const addLeave = createAction(ADD_LEAVE, props<any>());
+
+export const loadLeave = createAction(LOAD_LEAVE, props<any>());
+
+export const updateLeave = createAction(UPDATE_LEAVE, props<any>());
